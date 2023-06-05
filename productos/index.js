b1e29598881a255433785b0fb31214ce9d0ecacd
LuckyJorge <jorgefalcon71@gmail.com>
2023-06-04 23:08:54 -0300

    reparado error de exportacion de filtrar

--- productos/index.js
+++ productos/index.js
@@ -3,7 +3,7 @@ const Producto = require('./Producto');
 
 module.exports = {
     productos: leerJSON(),
-    filtrar: require('./filtar'),
+    filtrar: require('./filtrar'),
     listar: require('./listar'),
     agregar: require('./agregar'),
     //   editar : require('./editar'),
